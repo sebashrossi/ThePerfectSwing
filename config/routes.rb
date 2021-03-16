5e73beb4e22c580980dd059063fa3f46c2575550
Sebas Rossi <sebashrossi@gmail.com>
2021-03-16 14:23:58 +0100

    dashboard init

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get 'training/edit'
   get 'training/update'
   get 'training/destroy'
+  get '/dashboard', to: 'pages#dashboard', as: :dashboard
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
