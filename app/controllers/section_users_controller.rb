c78cb513c713e47204186248d790797b48d88aa2
coleman gidcumb <colemangidcumb@gmail.com>
2021-03-16 08:36:18 -0600

    adjusted section show and added section_users controller

---
+++ app/controllers/section_users_controller.rb
@@ -0,0 +1,13 @@
+class SectionUsersController < ApplicationController
+  def new
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -29,13 +29,13 @@ company2 = Company.create!(name: 'Heck', address: 'Hululu boulevard', user: user
 user2.company = company
 user2.save!
 training2 = Training.create!(name: 'Training 404', description: 'Loerm Impsum', department: 'Software engeneer', company: company)
-section2 = Section.create!(name: 'Safety', length: 3, description: 'blabla', video_url: 'http://www.youtube.com', training: training)
+section2 = Section.create!(name: 'Safety', length: 3, description: 'blabla', video_url: 'https://www.youtube.com/watch?v=cpxI9eQdeec', training: training)
 questions2 = Question.create!(content: "What's the best fruit?", answer: 'banana', possible_answers: ['mango', 'apple'], section: section)
 training2_user2 = TrainingUser.create!(user: user2, training: training2)
 section2_user2 = SectionUser.create!(user: user2, section: section2, quiz_score: 90)
 
 training3 = Training.create!(name: 'Training 406', description: 'Loerm Impsum', department: 'Software engeneer', company: company)
-section3 = Section.create!(name: 'Safety', length: 3, description: 'blabla', video_url: 'http://www.youtube.com', training: training)
+section3 = Section.create!(name: 'Safety', length: 3, description: 'blabla', video_url: 'https://www.youtube.com/watch?v=cpxI9eQdeec', training: training)
 questions3 = Question.create!(content: "What's the best fruit?", answer: 'banana', possible_answers: ['mango', 'apple'], section: section)
 training3_user2 = TrainingUser.create!(user: user2, training: training3)
 section3_user2 = SectionUser.create!(user: user2, section: section3, quiz_score: 90)
