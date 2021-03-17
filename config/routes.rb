42e137ad68881ff8746f15c136eca7158aa9e744
Andrew Medhat <skiresstranger@gmail.com>
2021-03-17 19:30:26 +0200

    login page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/dashboard', to: 'pages#dashboard', as: :dashboard
   devise_for :users
-  root to: 'pages#home'
+  root to: 'sessions#new'
   resources :trainings
   resources :sections do
     member do
