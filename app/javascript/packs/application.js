6bc604791fff1b1db12735d44e21f73f9b03d54a
Sebas Rossi <sebashrossi@gmail.com>
2021-03-18 15:06:39 +0100

    clickeable features

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,11 +24,16 @@ require("channels")
 
 // External imports
 import "bootstrap";
-
+import { completedTasks } from 'plugins/tasks.js';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  $('.popover-dismiss').popover({
+  trigger: 'focus'
+  });
+
+  completedTasks();
 });
